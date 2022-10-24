4712f61c00ad43e50b3005621d7eb0b10b9d7810
Lucas <luckslovez@gmail.com>
2022-10-24 11:04:05 +0200

    Use `long.js`

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 // index.js
 const protobuf = require('protobufjs');
+const long = require('long');
 
 protobuf.load("awesome.proto", function (err, root) {
     const AwesomeMessage = root.lookupType("awesomepackage.AwesomeMessage");
-    const payload = {awesomeNum: 1666189808901000000};
+    const awesomeNum = long.fromString("1666189808901000000");
+    const payload = {awesomeNum};
 
     const message = AwesomeMessage.create(payload);
     console.log(JSON.stringify(message)); //output: { awesomeNum: 1666189808901000000 }
@@ -11,5 +13,6 @@ protobuf.load("awesome.proto", function (err, root) {
     const buffer = AwesomeMessage.encode(message).finish();
 
     const decodedMessage = AwesomeMessage.decode(buffer);
-    console.log(JSON.stringify(decodedMessage)); //output: { awesomeNum: 1666189808900999936 }
+    console.log(JSON.stringify(decodedMessage)); //output: { awesomeNum: 1666189808901000000 }
+    // ALL GOOD! https://github.com/protobufjs/protobuf.js/issues/1827
 });
